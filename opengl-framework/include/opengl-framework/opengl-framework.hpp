b95a936c2d207873a2572f273eba1e962747ac6d
Jules Fouchy <jules.fouchy@ntymail.com>
2024-05-03 00:17:59 +0200

    ⬆ [opengl-framework]

--- opengl-framework/include/opengl-framework/opengl-framework.hpp
+++ opengl-framework/include/opengl-framework/opengl-framework.hpp
@@ -29,6 +29,7 @@ auto window_height_in_screen_coordinates() -> int;
 auto window_aspect_ratio() -> float;
 
 auto time_in_seconds() -> float;
+auto delta_time_in_seconds() -> float;
 
 void bind_default_shader();
 auto sphere_vertices();
