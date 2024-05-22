81a15da29d5fb2154abb8d7ab817e18b971cfe7a
Bukibarak <bukibarak@gmail.com>
2024-05-22 10:35:19 +0200

    Rectangle (déjà avec l'index buffer pck j'ai déjà fait l'exo mais pas commit)

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,31 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
-    auto const triangle_mesh = gl::Mesh{{
+    // auto const triangle_mesh = gl::Mesh{{
+    //     .vertex_buffers = {{
+    //         .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+    //         .data   = {
+    //             -1.f, -1.f, // Position2D du 1er sommet
+    //             +1.f, -1.f, // Position2D du 2ème sommet
+    //              0.f, +1.f  // Position2D du 3ème sommet
+    //         },
+    //     }},
+    // }};
+
+    auto const rectangle_mesh = gl::Mesh{{
         .vertex_buffers = {{
             .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
             .data   = {
-                -1.f, -1.f, // Position2D du 1er sommet
-                +1.f, -1.f, // Position2D du 2ème sommet
-                 0.f, +1.f  // Position2D du 3ème sommet
+                -0.5f, 0.5f, // Top left
+                0.5f, 0.5f, // Top right
+                0.5f, -0.5f, // Bottom right
+                -0.5f, -0.5f, // Bottom left
             },
         }},
+        .index_buffer = {
+            0, 1, 2,
+            0, 2, 3
+        }
     }};
 
     while (gl::window_is_open())
@@ -31,6 +47,6 @@ int main()
         gl::bind_default_shader();
 
         // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
-        triangle_mesh.draw();
+        rectangle_mesh.draw();
     }
 }
