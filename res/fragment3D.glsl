cdc6a55d8e507314828fe4c2173060ef07906099
Bukibarak <bukibarak@gmail.com>
2024-05-22 12:28:22 +0200

    UV (still in main3D.cpp)

--- res/fragment3D.glsl
+++ res/fragment3D.glsl
@@ -1,11 +1,14 @@
 #version 410
 
+
+
 in vec3 vertex_position;
+in vec2 uv;
 
 out vec4 out_color;
 
 
 void main()
 {
-    out_color = vec4(vertex_position, 1.);
+    out_color = vec4(uv.x, uv.y, 0., 1.);
 }
