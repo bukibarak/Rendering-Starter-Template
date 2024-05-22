2b5c9a9400b13d6952246dc8a663f6050462ec08
Bukibarak <bukibarak@gmail.com>
2024-05-22 11:44:26 +0200

    3D Cube

--- res/vertex3D.glsl
+++ res/vertex3D.glsl
@@ -2,9 +2,9 @@
 
 uniform mat4 view_projection_matrix;
 
-layout(location = 0) in vec2 in_position;
+layout(location = 0) in vec3 in_position;
 
 void main()
 {
-    gl_Position = view_projection_matrix * vec4(in_position, 0., 1.);
+    gl_Position = view_projection_matrix * vec4(in_position, 1.);
 }
--- src/main3D.cpp
+++ src/main3D.cpp
@@ -24,27 +24,67 @@ void main() {
         },
     });
 
-    auto const rectangle_mesh = gl::Mesh{{
-        .vertex_buffers = {{
-            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
-            .data   = {
-                -0.5f, 0.5f, // Top left
-                0.5f, 0.5f, // Top right
-                0.5f, -0.5f, // Bottom right
-                -0.5f, -0.5f, // Bottom left
-            },
-        }},
-        .index_buffer = {
-            0, 1, 2,
-            0, 2, 3
-        }
-    }};
+    // auto const rectangle_mesh = gl::Mesh{{
+    //     .vertex_buffers = {{
+    //         .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+    //         .data   = {
+    //             -0.5f, 0.5f, // Top left
+    //             0.5f, 0.5f, // Top right
+    //             0.5f, -0.5f, // Bottom right
+    //             -0.5f, -0.5f, // Bottom left
+    //         },
+    //     }},
+    //     .index_buffer = {
+    //         0, 1, 2,
+    //         0, 2, 3
+    //     }
+    // }};
+
+    auto const cube_mesh = gl::Mesh{
+        {
+            .vertex_buffers = {
+                {
+                    .layout = {gl::VertexAttribute::Position3D{0}},
+                    .data = {
+                        -1.f,  1.f,  1.f,
+                         1.f,  1.f,  1.f,
+                        -1.f, -1.f,  1.f,
+                         1.f, -1.f,  1.f,
+                        -1.f,  1.f, -1.f,
+                         1.f,  1.f, -1.f,
+                        -1.f, -1.f, -1.f,
+                         1.f, -1.f, -1.f,
+                    },
+                }},
+            .index_buffer = {
+                0, 1, 2,
+                1, 3, 2,
+
+                4, 5, 6,
+                5, 7, 6,
+
+                4, 0, 6,
+                0, 2, 6,
+
+                1, 5, 3,
+                5, 7, 3,
+
+                4, 5, 7,
+                4, 1, 0,
+
+                6, 7, 3,
+                6, 3, 2,
+            }
+        }};
 
     auto const screen_shader = gl::Shader{{
         .vertex = gl::ShaderSource::File{"res/vertex3D.glsl"},
         .fragment = gl::ShaderSource::File{"res/fragment3D.glsl"},
     }};
 
+    // glEnable(GL_CULL_FACE);
+    // glCullFace(GL_FRONT);
+    // glFrontFace(GL_CW);
 
     // Update function
     while (gl::window_is_open()) {
@@ -55,21 +95,23 @@ void main() {
             gl::framebuffer_aspect_ratio() /*aspect ratio*/,
             0.000001f /*near plane*/
         );
-        // glm::mat4 const ortho_matrix = glm::ortho(-2.0f, +2.0f, -1.5f, +1.5f, 0.1f, 100.0f);
+        glm::mat4 const ortho_matrix = glm::ortho(-2.0f, +2.0f, -1.5f, +1.5f, 0.1f, 100.0f);
 
         glm::mat4 const translation_matrix = glm::translate(
             glm::mat4{1.f},
-            glm::vec3{0.5f * sin(gl::time_in_seconds()), 0.f, -0.2f} /* déplacement */
+            //glm::vec3{0.5f * sin(gl::time_in_seconds()), 0.f, -0.2f} /* déplacement */
+            glm::vec3{-2.f, 0.f, -1.f}
         );
         glm::mat4 const rotation_matrix = glm::rotate(
             glm::mat4{1.f},
-            gl::time_in_seconds() /*angle de la rotation*/,
+            // gl::time_in_seconds() /*angle de la rotation*/,
+            0.f,
             glm::vec3{0.f, 0.f, 1.f} /* axe autour duquel on tourne */
         );
-        glm::mat4 const model_matrix = translation_matrix * rotation_matrix;
+        glm::mat4 const model_matrix = rotation_matrix * translation_matrix;
 
 
-        glm::mat4 const view_projection_matrix =  model_matrix * projection_matrix * view_matrix;
+        glm::mat4 const view_projection_matrix =  projection_matrix * view_matrix * model_matrix;
 
         // Clear previous frame buffers
         glClear(GL_COLOR_BUFFER_BIT);
@@ -81,7 +123,7 @@ void main() {
 
         screen_shader.bind();
         screen_shader.set_uniform("view_projection_matrix", view_projection_matrix);
-        rectangle_mesh.draw();
+        cube_mesh.draw();
 
     }
 }
