e02e117f6983df81d03c3aaa9057f51944b10cd7
Bukibarak <bukibarak@gmail.com>
2024-05-22 15:03:32 +0200

    render target and post process

--- src/main3D.cpp
+++ src/main3D.cpp
@@ -38,6 +38,19 @@ void main() {
             },
         },
     }};
+    auto const texture = gl::Texture{
+        gl::TextureSource::File{ // Peut être un fichier, ou directement un tableau de pixels
+            .path           = "res/texture.png",
+            .flip_y         = true, // Il n'y a pas de convention universelle sur la direction de l'axe Y. Les fichiers (.png, .jpeg) utilisent souvent une direction différente de celle attendue par OpenGL. Ce booléen flip_y est là pour inverser la texture si jamais elle n'apparaît pas dans le bon sens.
+            .texture_format = gl::InternalFormat::RGBA8, // Format dans lequel la texture sera stockée. On pourrait par exemple utiliser RGBA16 si on voulait 16 bits par canal de couleur au lieu de 8. (Mais ça ne sert à rien dans notre cas car notre fichier ne contient que 8 bits par canal, donc on ne gagnerait pas de précision). On pourrait aussi stocker en RGB8 si on ne voulait pas de canal alpha. On utilise aussi parfois des textures avec un seul canal (R8) pour des usages spécifiques.
+        },
+        gl::TextureOptions{
+            .minification_filter  = gl::Filter::Linear, // Comment on va moyenner les pixels quand on voit l'image de loin ?
+            .magnification_filter = gl::Filter::Linear, // Comment on va interpoler entre les pixels quand on zoom dans l'image ?
+            .wrap_x               = gl::Wrap::Repeat,   // Quelle couleur va-t-on lire si jamais on essaye de lire en dehors de la texture ?
+            .wrap_y               = gl::Wrap::Repeat,   // Idem, mais sur l'axe Y. En général on met le même wrap mode sur les deux axes.
+        }
+    };
 
     // Camera initialization
     auto camera = gl::Camera{};
@@ -52,26 +65,6 @@ void main() {
             },
         },
     });
-
-    // auto const rectangle_mesh = gl::Mesh{{
-    //     .vertex_buffers = {{
-    //         .layout = {
-    //             gl::VertexAttribute::Position2D{0},
-    //             gl::VertexAttribute::UV{1}
-    //         },
-    //         .data   = {
-    //             -0.5f,  0.5f, 0.f, 0.f, // Top left
-    //              0.5f,  0.5f, 0.f, 1.f, // Top right
-    //              0.5f, -0.5f, 1.f, 1.f, // Bottom right
-    //             -0.5f, -0.5f, 1.f, 0.f, // Bottom left
-    //         },
-    //     }},
-    //     .index_buffer = {
-    //         0, 1, 2,
-    //         0, 2, 3
-    //     }
-    // }};
-
 
 
     auto const cube_mesh = gl::Mesh{
@@ -113,24 +106,34 @@ void main() {
             }
         }};
 
-    auto const texture = gl::Texture{
-        gl::TextureSource::File{ // Peut être un fichier, ou directement un tableau de pixels
-            .path           = "res/texture.png",
-            .flip_y         = true, // Il n'y a pas de convention universelle sur la direction de l'axe Y. Les fichiers (.png, .jpeg) utilisent souvent une direction différente de celle attendue par OpenGL. Ce booléen flip_y est là pour inverser la texture si jamais elle n'apparaît pas dans le bon sens.
-            .texture_format = gl::InternalFormat::RGBA8, // Format dans lequel la texture sera stockée. On pourrait par exemple utiliser RGBA16 si on voulait 16 bits par canal de couleur au lieu de 8. (Mais ça ne sert à rien dans notre cas car notre fichier ne contient que 8 bits par canal, donc on ne gagnerait pas de précision). On pourrait aussi stocker en RGB8 si on ne voulait pas de canal alpha. On utilise aussi parfois des textures avec un seul canal (R8) pour des usages spécifiques.
-        },
-        gl::TextureOptions{
-            .minification_filter  = gl::Filter::Linear, // Comment on va moyenner les pixels quand on voit l'image de loin ?
-            .magnification_filter = gl::Filter::Linear, // Comment on va interpoler entre les pixels quand on zoom dans l'image ?
-            .wrap_x               = gl::Wrap::Repeat,   // Quelle couleur va-t-on lire si jamais on essaye de lire en dehors de la texture ?
-            .wrap_y               = gl::Wrap::Repeat,   // Idem, mais sur l'axe Y. En général on met le même wrap mode sur les deux axes.
+    auto const screen_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {
+                gl::VertexAttribute::Position2D{0 },
+                gl::VertexAttribute::UV{1 }
+            },
+            .data   = {
+                -1.f, 1.f, 0.f, 1.f, // Top left
+                1.f, 1.f, 1.f, 1.f, // Top right
+                1.f, -1.f, 1.f, 0.f, // Bottom right
+                -1.f, -1.f, 0.f, 0.f, // Bottom left
+            },
+        }},
+        .index_buffer = {
+            0, 1, 2,
+            0, 2, 3
         }
-    };
+    }};
 
 
-    auto const screen_shader = gl::Shader{{
+    auto const camera_shader = gl::Shader{{
         .vertex = gl::ShaderSource::File{"res/vertex3D.glsl"},
         .fragment = gl::ShaderSource::File{"res/fragment3D.glsl"},
+    }};
+
+    auto const screen_shader = gl::Shader{{
+        .vertex = gl::ShaderSource::File{"res/vertexScreen3D.glsl"},
+        .fragment = gl::ShaderSource::File{"res/fragmentScreen3D.glsl"},
     }};
 
     // Only draw CW triangle front face
@@ -168,9 +171,9 @@ void main() {
 
         glm::mat4 const view_projection_matrix =  projection_matrix * view_matrix * model_matrix;
 
-        screen_shader.bind();
-        screen_shader.set_uniform("view_projection_matrix", view_projection_matrix);
-        screen_shader.set_uniform("tex", texture);
+        camera_shader.bind();
+        camera_shader.set_uniform("view_projection_matrix", view_projection_matrix);
+        camera_shader.set_uniform("tex", texture);
 
 
 
@@ -184,6 +187,9 @@ void main() {
             cube_mesh.draw();
         });
 
+        screen_shader.bind();
+        screen_shader.set_uniform("tex", render_target.color_texture(0)); // TODO DEMANDER SI C'EST JUSTE
+        screen_mesh.draw();
 
     }
 }
