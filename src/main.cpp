d62461e900c129808c1439edf8ba1f5be334e46a
Bukibarak <bukibarak@gmail.com>
2024-05-22 10:49:16 +0200

    [BONUS] Fade effect

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "opengl-framework/opengl-framework.hpp" // Inclue la librairie qui va nous servir à faire du rendu
 
-int main()
+void main()
 {
     // Initialisation
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
@@ -33,14 +33,43 @@ int main()
         }
     }};
 
+    auto const screen_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+            .data   = {
+                -1.f, 1.f, // Top left
+                1.f, 1.f, // Top right
+                1.f, -1.f, // Bottom right
+                -1.f, -1.f, // Bottom left
+            },
+        }},
+        .index_buffer = {
+            0, 1, 2,
+            0, 2, 3
+        }
+    }};
+
     auto const rect_shader = gl::Shader{{
         .vertex   = gl::ShaderSource::File{"res/vertex.glsl"},
         .fragment = gl::ShaderSource::File{"res/fragment.glsl"},
     }};
 
+    auto const screen_shader = gl::Shader{{
+            .vertex = gl::ShaderSource::File{"res/vertexScreen.glsl"},
+            .fragment = gl::ShaderSource::File{"res/fragmentScreen.glsl"},
+    }};
+
+    // Allow alpha
+    glEnable(GL_BLEND);
+
+    // On peut configurer l'équation qui mélange deux couleurs, comme pour faire différents blend mode dans Photoshop. Cette équation-ci donne le blending "normal" entre pixels transparents.
+    glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_ONE);
+
+
+
     while (gl::window_is_open())
     {
-        // Rendu à chaque frame
+        // RENDU A CHAQUE FRAME
 
         // Clear previous frame buffers
         // glClear(GL_COLOR_BUFFER_BIT);
@@ -48,10 +77,7 @@ int main()
         // glClear(GL_STENCIL_BUFFER_BIT);
 
         // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
-        glClearColor(0.f, 0.f, 1.f, 1.f);
-
-        // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
-        glClear(GL_COLOR_BUFFER_BIT);
+        // glClearColor(0.f, 0.f, 1.f, 1.f);
 
         // Bind shader with screen
         rect_shader.bind();
@@ -62,5 +88,15 @@ int main()
 
         // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
         rectangle_mesh.draw();
+
+
+        // Bind screen shader
+        screen_shader.bind();
+
+        // Draw screen shader with very low alpha
+        screen_mesh.draw();
+
+
+
     }
 }
