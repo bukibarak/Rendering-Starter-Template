844fbd507b4aae917a7fd7ab95402c9d6fa6497a
Bukibarak <bukibarak@gmail.com>
2024-06-14 12:35:02 +0200

    show OBJ normals

--- res/fragmentOBJ.glsl
+++ res/fragmentOBJ.glsl
@@ -2,12 +2,17 @@
 
 uniform sampler2D tex;
 
+in vec3 normals;
 in vec2 uv;
+
 out vec4 out_color;
 
 
 void main()
 {
-    vec4 tex_color = texture(tex, uv);
-    out_color = tex_color;
+//    vec4 tex_color = texture(tex, uv);
+//    out_color = tex_color;
+
+    vec4 normals_color = vec4(normals, 1.);
+    out_color = normals_color;
 }
--- res/vertexOBJ.glsl
+++ res/vertexOBJ.glsl
@@ -3,12 +3,15 @@
 uniform mat4 view_projection_matrix;
 
 layout(location = 0) in vec3 in_position;
+layout(location = 1) in vec3 in_normals;
 layout(location = 2) in vec2 in_uv;
 
+out vec3 normals;
 out vec2 uv;
 
 void main()
 {
+    normals = in_normals;
     uv = in_uv;
     gl_Position = view_projection_matrix * vec4(in_position, 1.);
 }
